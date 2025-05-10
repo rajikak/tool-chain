1fb0abd9f62de737c2c16f53228f41ffe37299c4
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2025-05-10 17:17:24 -0500

    hex dump tool

--- src/hexdump.rs
+++ src/hexdump.rs
@@ -1,28 +1,26 @@
+use std::fmt::format;
 use std::fs::File;
 use std::io::Read;
-use crate::error;
 
 #[derive(Debug)]
-pub struct Hex {
-    file:String, 
-} 
+pub struct Hexer {
+    buf: Vec<u8>,
+}
 
-impl Hex {
-    
-    // print a binary file in hexadecimal 
+impl Hexer {
+    // print a binary file in hexadecimal
     // https://github.com/torvalds/linux/blob/master/lib/hexdump.c
-    
-    pub fn new(file: String) -> Self {
-        Self { file }
-    }
-    
-    pub fn print(&self, n:u8) {
-        let mut buf = Vec::new();
-        let mut file = File::open(&self.file).unwrap_or_else(|_| error("opening"));
-        file.read_to_end(&mut buf).unwrap_or_else(|_| error("reading"));
-        buf.reverse();
 
-        print!("{:#x}, {:#x}, {:#x}, {:#x}, {:#x}", buf.pop().unwrap(), buf.pop().unwrap(), buf.pop().unwrap(), buf.pop().unwrap(), buf.pop().unwrap());
+    pub fn new(buf: Vec<u8>) -> Self {
+        Self { buf }
+    }
+
+    pub fn hex(&self) -> String {
+        let s: Vec<String> = self.buf.chunks(2).map(|c| {
+            if c.len() == 2 {format!("{:02x}{:02x}", c[0], c[1])}
+            else {format!("{:02x}", c[0])}
+        }).collect();
         
+        s.join(" ")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
-use std::fs::File;
-use std::io::{Read};
+use crate::hexdump::{Hexer};
 use clap::Parser;
-use crate::hexdump::Hex;
+use std::fs::File;
+use std::io::Read;
 
 mod elf;
-mod macho;
-mod parser;
 mod hexdump;
-
-fn error(action:&str) -> ! {
-    eprintln!("something went wrong {} that file.", action);
-    std::process::exit(1)
-}
+mod macho;
 
 #[derive(Parser, Debug)]
 #[command(about, long_about=None)]
 struct Args {
-    #[arg(short, default_value="hexdump")]
-    tool:String,
+    #[arg(short, default_value = "hexdump")]
+    tool: String,
 
     #[arg(short)]
-    file:String,
+    file: String,
 }
 
 fn main() {
-
     let args = Args::parse();
-    
+
+    let mut buf = Vec::new();
+    match File::open(&args.file) {
+        Ok(mut file) => match file.read_to_end(&mut buf) {
+            Ok(_) => buf.reverse(),
+            Err(e2) => {
+                eprintln!("error reading file content: {}", e2);
+                std::process::exit(1)
+            }
+        },
+        Err(e) => {
+            eprintln!("error opening file: {}", e);
+            std::process::exit(1)
+        }
+    }
+
     if args.tool.trim() == "hexdump" {
-        let hex = Hex::new(args.file);
-        hex.print(10);
+        let hex = Hexer::new(buf);
+        let s = hex.hex();
+        println!("{}", s)
     } else {
         println!("not a supported tool, should be one of: hexdump, ldd, strings, strip.");
         return;
