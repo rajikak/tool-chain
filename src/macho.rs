929570f8cc719c3295ff5560b5bbd6789c3a1241
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2025-05-17 15:08:00 -0500

    macho tool

--- src/macho.rs
+++ src/macho.rs
@@ -1,3 +1,10 @@
+
+pub const MAGIC:u32 = 0xfeed_face; // magic field - 32 bit https://github.com/apple/darwin-xnu/blob/main/EXTERNAL_HEADERS/mach-o/loader.h#L65
+pub const CIGAM:u32 = 0xcdfa_edfe; // reverse 
+
+pub const MAGIC64:u32 = 0xfeed_facf; // magic field - 64 bit https://github.com/apple/darwin-xnu/blob/main/EXTERNAL_HEADERS/mach-o/loader.h#L84
+pub const CIGAM64:u32 = 0xcffa_edfe; // reverse
+
 #[derive(Debug)]
 pub struct MachoFile {
     buf: Vec<u8>,
@@ -11,7 +18,7 @@ impl MachoFile {
     pub fn new(buf: Vec<u8>) -> Self {
         Self {
             buf,
-            header: MachoHeader::new(54),
+            header: MachoHeader::new("".to_string()),
             commands: Vec::new(),
             segments: Vec::new(),
         }
@@ -20,19 +27,20 @@ impl MachoFile {
     pub fn print_magic(&mut self) {
         self.buf.reverse();
         let mut magic: Vec<u8> = self.buf.drain(0..4).collect();
+
         let str = format!("{:02x}{:02x}{:02x}{:02x}", magic.pop().unwrap(), magic.pop().unwrap(), magic.pop().unwrap(), magic.pop().unwrap());
-        println!("0x{}", str);
+        println!("magic: 0x{}", str);
     }
 }
 
 #[derive(Debug)]
 pub struct MachoHeader {
     // https://github.com/apple/darwin-xnu/blob/main/EXTERNAL_HEADERS/mach-o/loader.h#L72
-    pub magic: u32,
+    pub magic: String,
 }
 
 impl MachoHeader {
-    pub fn new(magic: u32) -> Self {
+    pub fn new(magic: String) -> Self {
         Self { magic }
     }
 }
