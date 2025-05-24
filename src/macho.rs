c813e6ef8a5f1d94643d1e9c4b67ded03e4d9eb3
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2025-05-23 20:09:13 -0500

    macho tool

--- src/macho.rs
+++ src/macho.rs
@@ -39,45 +39,95 @@ pub struct MachHeader {
     // https://github.com/apple/darwin-xnu/blob/main/EXTERNAL_HEADERS/mach-o/loader.h#L72
     pub magic: u32,
     pub endianness: Endianness,
+    pub cpu_type: u32,
+    pub cpu_sub_type: u32,
+}
+
+fn parse(input: &[u8]) -> Result<u32, &str> {
+    if input.len() != 4 {
+        return Err("input should be of size 4");
+    }
+    match u32::from_str_radix(
+        format!("{:x}{:x}{:x}{:x}", input[0], input[1], input[2], input[3]).as_str(),
+        16,
+    ) {
+        Ok(v) => Ok(v),
+        Err(e) => {
+            eprintln!("format error {}", e);
+            Err("error parsing input")
+        }
+    }
 }
 
 impl MachHeader {
     pub fn parse(input: &[u8]) -> Result<MachHeader, &str> {
-        let res = u32::from_str_radix(
-            format!("{:x}{:x}{:x}{:x}", input[0], input[1], input[2], input[3]).as_str(),
-            16,
-        );
-        if let Ok(MAGIC) = res {
+        let magic_no = parse(&input[0..3]);
+        let cpu_type =  parse(&input[4..8])?;
+        let cpu_sub_type = parse(&input[9..13])?;
+        if let Ok(MAGIC) = magic_no {
             let magic = MAGIC;
             let endianness = Endianness::Big;
-            Ok(MachHeader { magic, endianness })
-        } else if let Ok(MAGIC64) = res {
+            Ok(MachHeader {
+                magic,
+                endianness,
+                cpu_type,
+                cpu_sub_type,
+            })
+        } else if let Ok(MAGIC64) = magic_no {
             let magic = MAGIC64;
             let endianness = Endianness::Big;
-            Ok(MachHeader { magic, endianness })
-        } else if let Ok(CIGAM) = res {
+            Ok(MachHeader {
+                magic,
+                endianness,
+                cpu_type,
+                cpu_sub_type,
+            })
+        } else if let Ok(CIGAM) = magic_no {
             let magic = CIGAM;
             let endianness = Endianness::Little;
-            Ok(MachHeader{magic, endianness})
-        } else if let Ok(CIGAM64) = res {
+            Ok(MachHeader {
+                magic,
+                endianness,
+                cpu_type,
+                cpu_sub_type,
+            })
+        } else if let Ok(CIGAM64) = magic_no {
             let magic = CIGAM64;
             let endianness = Endianness::Little;
-            Ok(MachHeader{magic,endianness})
+            Ok(MachHeader {
+                magic,
+                endianness,
+                cpu_type,
+                cpu_sub_type,
+            })
         } else {
             Err("not a matching matcho header")
         }
     }
 
     pub fn str(&self) -> String {
-        let mut endianness = "";
+        let mut buf: Vec<String> = Vec::new();
+        buf.push("match header".to_string());
+
+        let mut endian_str: &str = "";
         if self.endianness == Endianness::Little {
-            endianness = "little"
+            endian_str = "little";
         } else if self.endianness == Endianness::Big {
-            endianness = "big"
+            endian_str = "big";
         } else {
-            endianness = "native"
+            endian_str = "native";
         }
-        format!("match header\nendianness: {}\nmagic: 0x{:x}", endianness, self.magic)
+
+        let endian = format!("endianness: {}", endian_str);
+        buf.push(endian);
+
+        let header = format!("magic: 0x{:x}", self.magic);
+        buf.push(header);
+
+        let cpu = format!("cpu_type: {:x}, sub_type: {:x}", self.cpu_type, self.cpu_sub_type);
+        buf.push(cpu);
+
+        buf.join("\n")
     }
 }
 
